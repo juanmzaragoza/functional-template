2d729f26676789c6703b707216c027a1563b9dab
Juan M Zaragoza <juanmanuelzar@gmail.com>
2018-09-20 17:15:55 -0300

    Modified excercise 1 guide 2 to support multimethod

--- src/exercise21.clj
+++ src/exercise21.clj
@@ -1,11 +1,15 @@
 (ns exercise21)
 
-(defn multimetodo-figuras
-  "Returns the :tipo of the figure."
-  [figure]
-  (str "Soy un " (figure :tipo))
- )
+(defmulti multimetodo-figuras
+	(fn[figure] (figure :tipo)))
 
+(defmethod multimetodo-figuras "figuras.rectangulo" [params]
+ "Soy un figuras.rectangulo")
 
+(defmethod multimetodo-figuras "figuras.cuadrado" [params]
+ "Soy un figuras.cuadrado")
+
+(defmethod  multimetodo-figuras :default [params]
+ "No soy una figura")
 
 
--- test/exercise21_test.clj
+++ test/exercise21_test.clj
@@ -2,14 +2,18 @@
   (:require [clojure.test :refer :all]
             [exercise21 :refer :all]))
 
+(def cuadrado {:nombre "cuadrado" :tipo "figuras.cuadrado"})
+(def rectangulo {:nombre "rectangulo" :tipo "figuras.rectangulo"})
+(def nofigura {:nombre "rectangulo" :tipo "figuras.rectangul2o"})
+
 (deftest figures-multimethod-show-reactangule-message
   (testing "Tests with a figure that return figuras.rectangulo."
-    (is (= (multimetodo-figuras (def cuadrado {:nombre "rectangulo" :tipo "figuras.rectangulo"})) "Soy un figuras.rectangulo"))))
+    (is (= (multimetodo-figuras rectangulo) "Soy un figuras.rectangulo"))))
 
 (deftest figures-multimethod-show-square-message
   (testing "Tests with a figure that return figuras.cuadrado"
-    (is (= (multimetodo-figuras (def cuadrado {:nombre "cuadrado" :tipo "figuras.cuadrado"})) "Soy un figuras.cuadrado"))))
+    (is (= (multimetodo-figuras cuadrado) "Soy un figuras.cuadrado"))))
 
 (deftest figures-multimethod-show-empty-value
-  (testing "Tests with a figure that return empty value"
-    (is (= (multimetodo-figuras (def cuadrado {:nombre "cuadrado" :tipo ""})) "Soy un "))))
+  (testing "Tests with a any value that it isn't a figure"
+    (is (= (multimetodo-figuras nofigura) "No soy una figura"))))
